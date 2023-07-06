dc8bd26d0cece1eee3fa74dfc87e17be74a77350
NJT123456 <jinjutha.njt@gmail.com>
2023-07-06 16:30:25 +0700

    master

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/TitleCaseSentence.js
+++
@@ -1,9 +0,0 @@
-function titleCase(str) {
-    var words = str.toLowerCase().split(" ");
-    var titleCaseWords = words.map(function (word) {
-        return word.charAt(0).toUpperCase() + word.slice(1);
-    });
-    return titleCaseWords.join(" ");
-}
-
-titleCase("I'm a little tea pot");
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Responsive-Web-Design
-from: https://www.freecodecamp.org/learn/2022/responsive-web-design/
+
+learning from: https://www.freecodecamp.org/learn/2022/responsive-web-design/
